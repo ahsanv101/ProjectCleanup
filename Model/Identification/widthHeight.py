103fadc60e6290f6693cc952d188aac1869fa17f
ahsanv101 <ahsansyedv101@gmail.com>
2020-02-05 21:28:37 +0500

    code

--- Model/Identification/widthHeight.py
+++ Model/Identification/widthHeight.py
@@ -34,14 +34,34 @@ def getWidthHeight(k):
     return width, height
 
 
+
+def pixelTomm(pixel,dpi):
+    return pixel*(25.5/dpi)
+    
+def getActualWidthHeight(height,width,dist,focal,dpi):
+    mmheight=pixelTomm(height,dpi)
+    mmwidth = pixelTomm(width,dpi)
+    h=(mmheight*dist)/focal
+    w=(mmwidth*dist)/focal
+    return w,h
+
 def main(filename):
     im = imageio.imread(filename)
     green = [0,255,0]
     colPixList = coloredPixels(im, green)
-    return getWidthHeight(colPixList)                                
-    
+    width, height =getWidthHeight(colPixList)
+    return getActualWidthHeight(height,width,2.28,30,320)
 
+#width is 825
+#height is 333
+#focal 30
+#dpi 320
+#dis 2.5
 
-width, height = main('ol.jpeg')
+width, height = main('output/o2.jpg')
 print("\nWidth of the garbage is:", width)
 print("\nHeight of the garbage is:", height)
+
+
+#width 0.55
+#height 1.524
