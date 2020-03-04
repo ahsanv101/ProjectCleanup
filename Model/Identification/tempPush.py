a47e93322e0ecc85ea99707d4a31e5dd164eafd3
Muhammad Tahir <mt02264@st.habib.edu.pk>
2020-03-04 18:29:16 +0500

    cloud upload DONE dana DONE done

---
+++ Model/Identification/tempPush.py
@@ -0,0 +1,167 @@
+import os
+import math
+import imageio
+import caffe
+from pylab import *
+from PIL import Image
+import numpy as np
+import cv2
+import time
+
+
+import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+from firebase_admin import db
+from widthHeight import *
+
+mean_filename='SpotGarbage_GarbNet/garbnet_mean.binaryproto'
+deploy_filename = 'SpotGarbage_GarbNet/deploy_garbnet.prototxt'
+caffemodel_file = 'SpotGarbage_GarbNet/garbnet_fcn.caffemodel'
+
+proto_data = open(mean_filename, "rb").read()
+a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
+mean  = caffe.io.blobproto_to_array(a)[0]
+
+net = caffe.Net(deploy_filename,caffemodel_file,caffe.TEST)
+
+
+
+def listener(event):
+    #print(event.event_type)  # can be 'put' or 'patch'
+
+    # print(event.path)  # relative to the reference, it seems
+    try:
+        url = event.data['imageUrl'].split('/')[7].split('?')[0].replace('%2F', '/')
+        #print(event.event_type)
+        #print(event.path)
+        ref=db.reference(event.path)
+        #ref.push({
+         #    'location': 'vini vidi vici',
+          #   'volume': '242.12354'
+         #})
+        print(ref.get())
+        bucket = storage.bucket()
+        blob = bucket.get_blob(url)
+        file = url.split('/')[1]
+        print(file,"\n\nthis is ")
+        blob.download_to_filename('input/'+file)
+        #specify 'input' folder containing images for prediction
+        images,names = gatherImages('input')
+        #specify 'output' folder to store segmented predictions
+        getPredictionsFor(images,names,4,0.999,'output')
+
+        for i in names:
+        	print('input/'+str(i))
+        	if os.path.exists('input/'+str(i)):
+        		os.remove('input/'+str(i))
+        	else:
+        		print("o lala la")
+        imagesO, nameO = gatherImages('output')
+        if len(imagesO) == 0 and len(nameO) == 0:
+        	pass
+        else:
+        	for i in nameO:
+        		x,y=mainCalculate('output/'+str(i),1.27,3.20,59)
+        		print("width: ",x)
+        		print("height: ", y)
+
+
+
+    except KeyError:
+        print("Starting...\n")
+
+cred = credentials.Certificate('ServiceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'ionicimageupload-977ea.appspot.com',
+    'databaseURL': 'https://ionicimageupload-977ea.firebaseio.com/'
+})
+
+
+# Get a database reference to our posts
+ref = db.reference().listen(listener)
+
+
+
+
+def gatherImages(folder,imageNames=None):
+    images = []
+    names = []
+    files = os.listdir(folder)
+    total = len(files)
+    print('Total %d images in folder %s' % (total,folder))
+    for i in os.listdir(folder):
+        try:
+            if imageNames is None or i in imageNames:
+                example_image = folder+'/'+i
+                input_image = Image.open(example_image)
+                images.append(input_image)
+                names.append(i)
+        except:
+            pass
+
+    return images,names
+
+def resizeForFCN(image,size):
+    w,h = image.size
+    if w<h:
+        return image.resize((int(227*size),int((227*h*size)/w))) #227x227 is input for regular CNN
+    else:
+        return image.resize((int((227*w*size)/h),int(227*size)))
+    
+def getSegmentedImage(test_image, probMap,thresh):
+    kernel = np.ones((6,6),np.uint8)
+    wt,ht = test_image.size
+    out_bn = np.zeros((ht,wt),dtype=uint8)
+    
+    for h in range(probMap.shape[0]):
+                for k in range(probMap.shape[1]):
+                    if probMap[h,k] > thresh:
+                        x1 = h*62 #stride 2 at fc6_gb_conv equivalent to 62 pixels stride in input
+                        y1 = k*62
+                        for hoff in range(x1,227+x1):
+                            if hoff < out_bn.shape[0]:
+                                for koff in range(y1,227+y1):
+                                    if koff < out_bn.shape[1]:
+                                        out_bn[hoff,koff] = 255
+    edge = cv2.Canny(out_bn,200,250)
+    box = cv2.dilate(edge,kernel,iterations = 3)
+    
+    or_im_ar = np.array(test_image)
+    or_im_ar[:,:,1] = (or_im_ar[:,:,1] | box)
+    or_im_ar[:,:,2] = or_im_ar[:,:,2] * box + or_im_ar[:,:,2]
+    or_im_ar[:,:,0] = or_im_ar[:,:,0] * box + or_im_ar[:,:,0]
+    
+    return Image.fromarray(or_im_ar)
+    
+    
+def getPredictionsFor(images,names,size,thresh,output_folder):
+    for i in range(len(images)):
+        try:
+            test_image = resizeForFCN(images[i],size)
+            
+            in_ = np.array(test_image,dtype = np.float32)
+            in_ = in_[:,:,::-1]
+            print(in_.shape)
+            
+            in_ -= np.array(mean.mean(1).mean(1))
+            in_ = in_.transpose((2,0,1))
+
+            net.blobs['data'].reshape(1,*in_.shape)
+            net.blobs['data'].data[...] = in_
+            net.forward()
+            
+            probMap =net.blobs['prob'].data[0,1]
+            print (names[i]+'...',)
+            if len(np.where(probMap>thresh)[0]) > 0:
+                print ('Garbage!')
+                out_ = getSegmentedImage(test_image, probMap,thresh)
+                out_.save(output_folder + '/output_' + names[i])
+            else:
+                print ('Not Garbage!')
+            
+
+        except:
+            pass
+
--- Model/Identification/widthHeight.py
+++ Model/Identification/widthHeight.py
@@ -1,3 +1,81 @@
+<<<<<<< HEAD
+import imageio
+import numpy as np
+import math
+
+
+def coloredPixels(im, col):
+    k = []
+    # print(im.shape)
+    for i in range(len(im)):
+        for j in range(len(im[0])):
+            if np.allclose(im[i][j], col):
+                k.append([i,j])
+        #print(i)
+    return k
+
+
+
+def getWidthHeight(k):
+    maxY = maxX = 0
+    minY = minX = 300
+    for i in k:
+        if i[1] > maxX:
+            maxX = i[1]
+        if i[1] < minX:
+            minX = i[1]
+        if i[0] > maxY:
+            maxY = i[0]
+        if i[0] < minY:
+            minY = i[0]
+    print(minX, minY)
+    print(maxX, maxY)
+
+    width = abs(maxX - minX)
+    height = abs(maxY - minY)
+    return width, height
+
+def getSensorWidth(horzifov,focal):
+    return math.tan(math.radians(horzifov/2))*2*focal
+
+def pixelTomm(pixel,dpi):
+    return pixel*25.4/dpi
+    
+def getActualWidth(width,dist,focal):
+    actualWidth=(width*dist)/focal
+    return actualWidth
+
+def getActualHeight(actualWidth,pixwidth,pixheight):
+    pixelPerMetric = pixwidth/actualWidth
+    return pixheight/pixelPerMetric
+
+def calculate(filename,dist,focal,horizfov):
+    im = imageio.imread(filename)
+    green = [0,255,0]
+    colPixList = coloredPixels(im, green)
+    pixwidth, pixheight =getWidthHeight(colPixList)
+    print(pixwidth,pixheight)
+    sensorw = getSensorWidth(horizfov,focal)
+    widthmm=pixwidth*sensorw/im.shape[1]
+    actualWidth=getActualWidth(widthmm,dist,focal)
+    actualHeight=getActualHeight(actualWidth,pixwidth,pixheight)
+    return actualWidth,actualHeight
+
+def mainCalculate(filename1,dist1,focal,horizfov):
+    x,y =calculate(filename1,dist1,focal,horizfov)
+    # z,w = calculate(filename2,dist2,focal,horizfov)
+    print("\nWidth of the filename1 is:", x)
+    print("\nHeight of the filename1 is:", y)
+    # print("\nWidth of the filename2 is:", z)
+    # print("\nHeight of the filename2 is:", w)
+    # return x * y * z
+    return x,y
+
+# volume = main('output/s1.jpg','output/s2.jpg',1.27,1.27,3.20,59)
+# print("\nVolume of the garbage is:", volume)
+
+
+=======
 import imageio
 import numpy as np
 import math
@@ -72,3 +150,4 @@ volume = main('output/s1.jpg','output/s2.jpg',1.27,1.27,3.20,59)
 print("\nVolume of the garbage is:", volume)
 
 
+>>>>>>> 03dcbf2a0f4feec4fba203c349b4a45eca8991f8
